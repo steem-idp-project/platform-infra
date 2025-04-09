2160fa544870926f95e5a5ae98e82dc8b89a6682
Gabi-Limberea <gabilimberea@gmail.com>
2025-04-09 14:43:39 +0300

    add config map for pgsql init

--- database/deploy-db.sh
+++ database/deploy-db.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# nfs_ip=$(kubectl get service nfs -o json | jq -r ".spec.clusterIP")
-# nfs_share=$(kubectl get deployments.apps nfs -o json | jq -r '.spec.template.spec.containers | .[0].volumeMounts | .[0].mountPath')
-
-# helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/ 2> /dev/null
-# helm install nfs-client nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
-#     --set nfs.server=$nfs_ip \
-#     --set nfs.path=/ \
-#     --values nfs/manifests/nfs-client-values.yaml
-
 manifest_dir="database/manifests"
 
 echo
@@ -18,6 +9,10 @@ kubectl apply -f $manifest_dir/postgres-data-pv.yaml
 echo
 echo "Apply data persistent volume claim"
 kubectl apply -f $manifest_dir/postgres-data-pvc.yaml
+
+echo
+echo "Apply init configmap for postgres"
+kubectl apply -f $manifest_dir/postgres-init-cfg.yaml
 
 echo
 echo "Apply postgres service"
