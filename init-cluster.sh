89c7b602e4b3e5aec6b38a974fb95bf3afb95a58
Gabi-Limberea <gabilimberea@gmail.com>
2025-04-24 19:27:45 +0300

    fix pgadmin deployment and add nginx

--- init-cluster.sh
+++ init-cluster.sh
@@ -8,11 +8,22 @@
 #     exit 1
 # fi
 
+pgsql_creds_file="database/manifests/template-postgres-user-creds.yaml"
+if [[ -n "$1" ]]; then
+    echo "Using provided postgres user creds file: $1"
+    pgsql_creds_file="$1"
+else
+    echo "Using default postgres user creds file: $pgsql_creds_file"
+fi
+
 # create the kubernetes with 1 control plane and 2 workers
 kind create cluster --config cluster.yaml
 
 kubectl wait --for=condition=Ready nodes --all --timeout=300s
 
+kubectl apply -f $pgsql_creds_file
+
 # deploy and initialize database
 ./database/deploy-db.sh
 ./pgadmin/deploy-pgadmin.sh
+./nginx/deploy-nginx.sh
---
+++ nginx/deploy-nginx.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Install Nginx Ingress Controller"
+helm repo add nginx https://helm.nginx.com/stable
+helm install steem nginx/nginx-ingress --version 2.1.0 -n nginx-ingress --create-namespace
+kubectl wait --for=condition=available --timeout=300s deployment/steem-nginx-ingress-controller -n nginx-ingress
+
+echo ""
+echo "Creating admin-steem tls secret"
+kubectl create secret tls admin-tls-certs \
+    --cert=nginx/manifests/admin-certs/admin.crt \
+    --key=nginx/manifests/admin-certs/admin.key
+
+echo ""
+echo "Applying Nginx ingress configuration"
+kubectl apply -f nginx/manifests/virtual-server-admin.yaml
+
+echo ""
+echo "In order to resolve idp.admin-steem.com and idp.steem.com, the external IP of the Nginx ingress controller must be added to the /etc/hosts file."
+echo "Checking if Cloud Provider Kind if running for external IP allocation"
+ps aux | grep cloud-provider-kind | grep -v grep | grep -v systemd > /dev/null
+if [[ "$?" -ne 0 ]]; then
+    echo "Cloud Provider Kind not found. After starting it, you can get the external IP IP with:"
+    echo "kubectl get svc -n nginx-ingress -o jsonpath='{.items[0].status.loadBalancer.ingress[0].ip}'"
+    exit 1
+else
+    echo "Cloud Provider Kind is running. External IP is:"
+    echo $(kubectl get svc -n nginx-ingress -o jsonpath='{.items[0].status.loadBalancer.ingress[0].ip}')
+fi
